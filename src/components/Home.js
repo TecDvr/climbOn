b2e85c94ffae152700b0c4efdec408e466e1bb68
Zach Gwirtz <zachgw@gmail.com>
2019-07-04 10:43:48 -0700

    update push

---
+++ src/components/Home.js
@@ -0,0 +1,12 @@
+import React from 'react';
+import ReactDOM from 'react-dom';
+
+export default class Home extends React.Component {
+    render() {
+        return (
+            <div>
+                <h2>Home Screen</h2>
+            </div>
+        );
+    }
+}
